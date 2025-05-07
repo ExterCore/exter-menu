b6bb99e9df87241c3bc4e71bf038329b7cc79efe
CtrlAltDelete4413 <95325881+CtrlAltDelete4413@users.noreply.github.com>
2025-05-07 11:18:08 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+ui_page 'html/index.html'
+escrow_ignore 'client/main.lua'
+client_scripts {
+	'client/main.lua'
+}
+files {
+    'html/**',
+}
+dependency '/assetpacks'
+dependency '/assetpacks'
